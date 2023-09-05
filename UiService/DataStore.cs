f1c92eb36ecf260ba27631e5de17c60c85c1ceca
Jörg Krause <joerg@joergkrause.de>
2023-09-05 11:15:33 +0200

    Daten in Queue

---
+++ UiService/DataStore.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace UiService
+{
+  public static class DataStore
+  {
+    [FunctionName("DataStore")]
+    public static void Run([BlobTrigger("files/{filename}.{extension}", Connection = "AzureWebJobsStorage")] Stream myBlob,
+      string filename,
+      string extension,
+      // [Blob("files/{filename}.pdf", FileAccess.Read, Connection = "AzureWebJobsStorage")] byte[] content,
+      [Queue("storage", Connection = "AzureWebJobsStorage")] ICollector<StorageItem> queueMessage,
+      ILogger log)
+    {
+      log.LogInformation($"C# Blob trigger function Processed blob\n Name:{filename}.{extension} \n Size: {myBlob.Length} Bytes");
+
+      // .txt .json .pdf
+      var blobContent = new byte[myBlob.Length];
+      myBlob.Read(blobContent, 0, (int)myBlob.Length);
+      var content = System.Text.Encoding.UTF8.GetString(blobContent);
+
+      queueMessage.Add(new StorageItem { Content = content, Name = filename, Created = DateTime.UtcNow, Status = 0 });
+    }
+  }
+}
---
+++ UiService/StorageItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiService
+{
+  public class StorageItem
+  {
+    public string Content { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime Created { get; set; }
+
+    public int Status { get; set; }
+  }
+}
